227018e04269d3e2cc6352262ef90878140e798e
prarthanaagrawal <agrawalprarthana75@gmail.com>
2024-05-03 12:47:30 +0000

    Create a portfolio for given below details Here is a little background Follow your heart and the rest will fall into place. The only true limitation is the one you set for yourself. My name is Prarthana Agrawal. I am a passionate front-end web developer, specifically, React.js. I became fascinated by the endless creative possibilities and the ability to build interactive and visually appealing web applications. I love using React to build user-friendly and interactive web applications. I threw myself into learning and improving my skills, and I am now a passionate front-end web developer with a strong foundation in HTML, CSS, and JavaScript. I am constantly learning and staying up-to-date with the latest technologies and best practices in the industry. Spend time solving coding problems on websites like LeetCode, HackerRank and CodeChef to build your problem-solving and coding skills. Take part in online coding competitions and hackathons to learn from other participants and to get comfortable with the competitive environment.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listeners to handle user interactions
+// Implement smooth scrolling functionality when navigating through the website
+// Make AJAX requests to the back-end to fetch project details and contact information
+// Update the HTML document with the fetched data
+// Implement responsive design using media queries to ensure the website looks and functions well on different devices and screen sizes
